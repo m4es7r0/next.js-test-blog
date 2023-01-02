0b87f5448ac50fefa01376c365985413d6275350
Dmytro Musiichenko <dmitrymusiichenko@gmail.com>
2023-01-02 15:45:55 +0200

    some code

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,27 @@
-import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
+import Link from "next/link";
+
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Posts | first post</title>
+        {/* <script defer src="https://connect.facebook.net/en_US/sdk.js" /> */}
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.info(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
-      <h2>
+      {/* <h2>
         <Link href="/">Back to home</Link>
-      </h2>
-    </>
+      </h2> */}
+    </Layout>
   );
 }
